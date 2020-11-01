03638cd3213553463fcf10cb04b34f6f3c58f5c6
MysticBoy <mysticboy@live.com>
2020-11-01 21:43:52 +0800

    增加 CAP.Extensions

---
+++ src/CAP.Extensions/CapOptionsExtensions.cs
@@ -0,0 +1,14 @@
+﻿using DotNetCore.CAP;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class CapOptionsExts
+    {
+        public static CapOptions UseRabbitMQ(this CapOptions options, Uri url)
+        {
+            return options.UseRabbitMQ(opt => opt.ConnectionFactoryOptions = factory => factory.Uri = url);
+        }
+    }
+}
